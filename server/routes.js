94c44e26f6ea54de6013ab3c16739e781007c6df
Clayton Jones <claytonjonesLMP@gmail.com>
2020-03-01 18:35:27 -0800

    added some barb info

--- server/routes.js
+++ server/routes.js
@@ -6,15 +6,35 @@ const superagent = require('superagent');
 // will be used to query our postgres DB
 const database = require('./database.js');
 
+const classesArr = require('../json/classes.json');
+
 // page routes
 const routes = {
-  homePage: homePage
+  homePage: homePage,
+  getClasses: getClasses,
+  getOneClass: getOneClass
 };
 
 function homePage (req, res) {
   res.render('index.ejs');
 }
 
+function getClasses (req, res) {
+  res.json(classesArr);
+}
+
+function getOneClass (req, res) {
+  const charRequest = req.params.class;
+  const classToSend = classesArr.filter(dndClass => {
+    if (dndClass.name.toLowerCase() === charRequest.toLowerCase()) {
+      return true;
+    } else {
+      return false;
+    }
+  })[0];
+
+  res.json(classToSend);
+}
 
 module.exports = routes;
 
